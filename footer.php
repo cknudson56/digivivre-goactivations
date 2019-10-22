c8703d8df16544e1f25aba89f907bb1809f9f2e6
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-10-21 19:45:46 -0500

    Add skeleton to footer

--- footer.php
+++ footer.php
@@ -1,30 +1,39 @@
 <footer>
-  <div>
-    <div>
-      <!--Logo/text widget-->
-      <?php dynamic_sidebar('left-footer'); ?>
-    </div>
+  <div class="container">
+    <div class="row">
+      <div class="three columns">
+        <!--Logo/text widget-->
+        <?php dynamic_sidebar('left-footer'); ?>
+      </div>
 
-    <div>
-      <!-- Navigation -->
-      <?php wp_nav_menu(array(
-        'theme_location' => 'footer-menu',
-        'container-class' => 'menu-footer'
-        ));
-      ?>
-    </div>
+      <div class="three columns">
+        <!-- Navigation -->
+        <?php wp_nav_menu(array(
+          'theme_location' => 'footer-menu',
+          'container-class' => 'menu-footer'
+          ));
+        ?>
+      </div>
 
-    <div>
-      <!--Contact Widget-->
-      <?php dynamic_sidebar('right-footer'); ?>
-      <!--Social Widget-->
-      <?php dynamic_sidebar('social-footer'); ?>
+      <div class="three columns">
+        <!-- Navigation -->
+        
+      </div>
+
+      <div class="three columns">
+        <!--Contact Widget-->
+        <?php dynamic_sidebar('right-footer'); ?>
+        <!--Social Widget-->
+        <?php dynamic_sidebar('social-footer'); ?>
+      </div>
     </div>
   </div>
 
-  <div>
-    <!--Copyright-->
-    <p>&copy; Site created by DigiVivre 2019</p>
+  <div class="row">
+    <div class="twelve columns">
+      <!--Copyright-->
+      <p>&copy; Site created by DigiVivre 2019</p>
+    <div>
   </div>
 </footer>
 
