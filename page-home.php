89d91df6207794bb15e6869d6e64e73284c31d8e
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-10-22 19:58:12 -0500

    Add skeleton to home page

--- page-home.php
+++ page-home.php
@@ -1,49 +1,59 @@
 <?php /*Template Name: Home Page Template*/ ?>
 <?php get_header(); ?>
 
-<div>
-  <div>
-    <!--Banner Img Widget-->
-    <?php dynamic_sidebar('banner-home'); ?>
+<div class="container">
+  <div class="row">
+    <div class="twelve columns">
+      <!--Banner Img Widget-->
+      <?php dynamic_sidebar('banner-home'); ?>
+    </div>
   </div>
-</div>
 
-<div>
-  <div>
-    <!-- About Brief Text Widget-->
-    <?php dynamic_sidebar('about-brief-home'); ?>
+  <div class="row">
+    <div class="six columns">
+      <!-- About Brief Text Widget-->
+      <?php dynamic_sidebar('about-brief-home'); ?>
+    </div>
+    <div class="six columns">
+      <!--Img Widget-->
+      <?php dynamic_sidebar('right-img-home'); ?>
+    </div>
   </div>
-  <div>
-    <!--Img Widget-->
-    <?php dynamic_sidebar('right-img-home'); ?>
+
+  <div class="row">
+    <div class="twelve columns">
+      <!--Small Banner Img Widget-->
+      <?php dynamic_sidebar('small-banner-home'); ?>
+    </div>
   </div>
-</div>
 
-<div>
-  <!--Small Banner Img Widget-->
-  <?php dynamic_sidebar('small-banner-home'); ?>
-</div>
+  <div class="row">
+    <div class="twelve columns">
+      <!--Service Brief Widgets-->
+      <?php dynamic_sidebar('service-brief-home'); ?>
+    </div>
+  </div>
 
-<div>
-  <!--Service Brief Widgets-->
-  <?php dynamic_sidebar('service-brief-home'); ?>
-</div>
+  <div class="row">
+    <!--Work Brands Slider Widget (Need to ask Dawne about brands and case studies)-->
+    <?php dynamic_sidebar(''); ?>
+  </div>
 
-<div>
-  <!--Work Brands Slider Widget (Need to ask Dawne about brands and case studies)-->
-  <?php dynamic_sidebar(''); ?>
-</div>
+  <div class="row">
+    <div class="six columns">
+      <!--Img Widget-->
+      <?php dynamic_sidebar('left-img-home'); ?>
+    </div>
+    <div class="six columns">
+      <!-- Contact Brief Text Widget-->
+      <?php dynamic_sidebar('contact-brief-home'); ?>
+    </div>
+  </div>
 
-<div>
-  <!--Img Widget-->
-  <?php dynamic_sidebar('left-img-home'); ?>
-  <!-- Contact Brief Text Widget-->
-  <?php dynamic_sidebar('contact-brief-home'); ?>
-</div>
-
-<div>
-  <!--Feature Posts/Case Studies Widget (Need to ask Dawne about brands and case studies)-->
-  <?php dynamic_sidebar(''); ?>
+  <div class="row">
+    <!--Feature Posts/Case Studies Widget (Need to ask Dawne about brands and case studies)-->
+    <?php dynamic_sidebar(''); ?>
+  </div>
 </div>
 
 <?php get_footer(); ?>
