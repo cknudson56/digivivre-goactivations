e2618fa51969b057bd89119ece4433e3fa2647d6
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-10-24 15:17:35 -0500

    Style Sidebar

--- page-about.php
+++ page-about.php
@@ -9,39 +9,40 @@
     </div>
   </div>
 
-<div class="section-margins">
-  <div class="row">
-    <div class="four columns">
-      <div class="title-box"></div>
-      <h2><?php the_title(); ?></h2>
+  <div class="section-margins">
+    <div class="row">
+      <div class="four columns">
+        <div class="title-box"></div>
+        <h2><?php the_title(); ?></h2>
+      </div>
+
+      <div class="eight columns">
+        <!--Big Text Widget-->
+        <?php dynamic_sidebar('bigtext-about'); ?>
+      </div>
     </div>
 
-    <div class="eight columns">
-      <!--Big Text Widget-->
-      <?php dynamic_sidebar('bigtext-about'); ?>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="twelve columns">
-      <!--Text-->
-      <?php
-        if(have_posts()){
-          while(have_posts()){
-            the_post();?>
-            <?php the_content();
+    <div class="row">
+      <div class="twelve columns">
+        <!--Text-->
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post();?>
+              <?php the_content();
+            }
           }
-        }
-      ?>
+        ?>
+      </div>
     </div>
   </div>
 </div>
 
-  <div class="row">
-    <div class="twelve columns">
-      <?php get_sidebar(); ?>
-    </div>
+<div id="sidebar" class="row">
+  <div class="twelve columns">
+    <?php get_sidebar(); ?>
   </div>
 </div>
+
 
 <?php get_footer(); ?>
