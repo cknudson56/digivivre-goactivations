82bd881e3dd208e4d96900d521a6fc3971b9f213
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-10-29 17:06:39 -0500

    Edit to footer-end

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
     <div class="row">
       <div class="twelve columns">
         <!--Copyright-->
-        <p>&copy; Site created by DigiVivre 2019</p>
+        <p class="center-text footer-end">&copy; Site created by DigiVivre 2019</p>
       </div>
     </div>
   </div>
