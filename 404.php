0c9bb37b50f87393acfa20c4174c8e25d43ee9a8
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-10-30 12:11:10 -0500

    Finalize 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,22 @@
 <?php /*Template Name: 404 Page*/?>
 <?php get_header(); ?>
 
-<div>
-  <h2>Page Not Found</h2>
-  <p>We apologize for any inconvenience.</p>
-  <p>Since it seems like since nothing was found here, would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
+<div class="container">
+  <div class="row">
+    <div class="twelve columns head-margin-top">
+    </div>
+  </div>
+
+  <div class="row section-margins">
+    <div class="twelve columns">
+      <div>
+        <div class="title-box"></div>
+        <h2>Page Not Found</h2>
+        <p>We apologize for any inconvenience.</p>
+        <p>Since it seems like since nothing was found here, would you like to go back <a href="<?php echo home_url('/');?>">home <i class="fas fa-arrow-circle-right"></i></a>?</p>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
