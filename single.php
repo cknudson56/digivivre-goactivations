e6b9fef5d6f65a97a8c2e330eba439eb12045f0b
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-10-30 12:12:59 -0500

    Basic template to single

--- single.php
+++ single.php
@@ -1,4 +1,25 @@
 <?php get_header(); ?>
 
+<div class="container">
+  <div class="row">
+    <div class="twelve columns head-margin-top">
+    </div>
+  </div>
+
+  <div class="row section-margins">
+    <div class="twelve columns">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <div class="title-box"></div>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content();
+          }
+        }
+      ?>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
