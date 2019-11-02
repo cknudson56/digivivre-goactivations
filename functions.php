8547a78f3dc83d3053a4b83e0aebb6ddfff4d27c
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-02 12:39:54 -0500

    Remove js files

--- functions.php
+++ functions.php
@@ -6,8 +6,6 @@ function example_theme_scripts(){
   wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css');
   //CSS
   wp_enqueue_style('main-styles', get_stylesheet_uri());
-  //Javascript
-  wp_enqueue_style('script-name', get_template_directory_uri() . '/js/script.js');
 }
 add_action('wp_enqueue_scripts' , 'example_theme_scripts');
 
