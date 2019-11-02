2ab0fb0e38b4b7d075b7d4e08e89b0fb3b002ac4
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-02 12:40:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php get_header(); ?>
 
-  <?php
-    if(have_posts()){
-      while(have_posts()){
-        the_post();?>
+<div class="container">
+  <div class="row">
+    <div class="twelve columns head-margin-top">
+    </div>
+  </div>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content();
-      }
-    }
-  ?>
+  <div class="row section-margins">
+    <div class="twelve columns">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <div class="title-box"></div>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content();
+          }
+        }
+      ?>
+    </div>
+  </div>
+</div>
+
+<div id="sidebar">
+  <div class="flex-sidebar">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
 
 <?php get_footer(); ?>
