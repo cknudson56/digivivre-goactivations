deee235cb41278406f0e24ec307563730d88fbe3
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-16 16:55:49 -0600

    Add margin and change feat img on single

--- single.php
+++ single.php
@@ -3,6 +3,14 @@
 <div class="container">
   <div class="row">
     <div class="twelve columns head-margin-top">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <?php the_post_thumbnail();
+          }
+        }
+      ?>
     </div>
   </div>
 
@@ -12,9 +20,10 @@
         if(have_posts()){
           while(have_posts()){
             the_post();?>
-            <?php the_post_thumbnail(); ?>
-            <div class="title-box"></div>
-            <h2><?php the_title(); ?></h2>
+            <div class="bottom-margin">
+              <div class="title-box"></div>
+              <h2><?php the_title(); ?></h2>
+            </div>
             <div class="single-post-img"><?php the_content();
           }
         }
