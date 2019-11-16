da676efd5d5a52817b391af2d4a8ec34af99aebb
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-16 16:26:45 -0600

    Add style to blot title links and read more link

--- index.php
+++ index.php
@@ -15,9 +15,10 @@
           while(have_posts()){
             the_post();?>
             <div class="six columns">
-              <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
               <?php the_post_thumbnail('medium'); ?>
+              <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
               <p><?php the_excerpt(); ?></p>
+              <p><a href="<?php the_permalink(); ?>">Read More...</a></p>
             </div>
           <?php }
         }
